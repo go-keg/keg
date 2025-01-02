5576d30b80a58ca9e641c4c051cf37f5ebfc0829
eiixy <990656271@qq.com>
2025-01-02 15:19:36 +0800

    fix: k8s deployment update-image cmd

--- cmd/keg/cmd/k8s/deployment/update_image.go
+++ cmd/keg/cmd/k8s/deployment/update_image.go
@@ -11,6 +11,12 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
+var tag string
+
+func init() {
+	updateImageCmd.Flags().StringVarP(&tag, "tag", "t", "latest", "Tag of the deployment to update")
+}
+
 var updateImageCmd = &cobra.Command{
 	Use:     "update-image",
 	Example: "keg k8s deployment update-image -n account-interface",
@@ -21,12 +27,7 @@ var updateImageCmd = &cobra.Command{
 		}
 		b := cfg.GetBranch(branch)
 		deploy := NewDeployments(b.Namespace, name, pkg.NewClientSet(b.KubeConfig))
-		tag, err := utils.GetTag(cfg)
-		if err != nil {
-			log.Fatalf("get tag err: %v", err)
-		}
-		image := fmt.Sprintf("%s/%s", cfg.ImageRegistry, name+tag)
-
+		image := fmt.Sprintf("%s/%s:%s", cfg.ImageRegistry, name, tag)
 		ts := time.Now().Unix() * 1000
 		deploy.Patch(types.StrategicMergePatchType, []byte(fmt.Sprintf(`{"spec":{"template":{"metadata":{"annotations":{"redeploy-timestamp":"%d"}},"spec":{"containers":[{"name":"%s","image":"%s"}]}}}}`, ts, deploy.Name, image)))
 		fmt.Printf("updateImage namespace: %s redeploy.timestamp: %d name: %s image: %s\n", deploy.Namespace, ts, deploy.Name, image)
